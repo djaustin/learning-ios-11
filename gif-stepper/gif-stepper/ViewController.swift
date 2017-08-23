de2b3bb685f5d4de1638a2ed05484a1492a3a646
Dan Austin <dja470@bham.ac.uk>
2017-08-23 09:45:30 +0100

    Update gif-stepper with play/pause and animation basics

--- gif-stepper/gif-stepper/ViewController.swift
+++ gif-stepper/gif-stepper/ViewController.swift
@@ -9,15 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var btnPlayPause: UIButton!
     var count = 0
+    var timer = Timer()
     @IBOutlet weak var imageView: UIImageView!
-    @IBAction func buttonClicked(_ sender: Any) {
-        if let image = getStillImageName(fromNumber: count) {
-            imageView.image = image
-            count += 1
-        } else {
-            count = 0
+    @IBAction func growClicked(_ sender: Any) {
+        UIView.animate(withDuration: 1) {
+
+
         }
+        UIView.animate(withDuration: 1, animations: {
+            let frame = self.imageView.frame
+            self.imageView.frame = CGRect(x: frame.minX, y: frame.minY, width: frame.width+100, height: frame.height+100)
+        }) { (finished) in
+            UIView.animate(withDuration: 1, animations: {
+                let frame = self.imageView.frame
+                self.imageView.frame = CGRect(x: frame.minX, y: frame.minY, width: frame.width-100, height: frame.height-100)
+            })
+        }
+        
+    }
+    @IBAction func slideClicked(_ sender: Any) {
+        imageView.center = CGPoint(x: imageView.center.x - 500, y: imageView.center.y)
+        UIView.animate(withDuration: 2) {
+            self.imageView.center = CGPoint(x: self.imageView.center.x + 500, y: self.imageView.center.y)
+        }
+    }
+    @IBAction func fadeClicked(_ sender: Any) {
+        imageView.alpha = 0
+        UIView.animate(withDuration: 1) {
+            self.imageView.alpha = 1
+        }
+    }
+    @IBAction func buttonClicked(_ sender: Any) {
+        if !timer.isValid{
+            timer = Timer.scheduledTimer(timeInterval: 0.13, target: self, selector: #selector(progressGif), userInfo: nil, repeats: true)
+            btnPlayPause.setTitle("Pause", for: UIControlState.normal)
+        } else {
+            timer.invalidate()
+            btnPlayPause.setTitle("Play", for: .normal)
+        }
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +59,15 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func progressGif(){
+        if let image = getStillImageName(fromNumber: count) {
+            imageView.image = image
+            count += 1
+        } else {
+            count = 0
+        }
     }
 
     func getStillImageName(fromNumber number:Int) -> UIImage?{
